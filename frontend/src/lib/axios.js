a6a0368db23c2bda861a05d018b696b214beb2de
shwetasable971 <chmcs2026.shweta.sable@gmail.com>
2026-03-08 14:13:03 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "https://college-notice-board-system-5ivo.onrender.com"
 });
 
 export default api;
